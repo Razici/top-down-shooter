67d5730fc6a7bed5d4fd93581d759879e07e4242
Razici <andronachir@gmail.com>
2019-04-24 18:03:17 +0300

    add borders

--- 2DD/Assets/scripts/bullet.cs
+++ 2DD/Assets/scripts/bullet.cs
@@ -4,7 +4,6 @@ using UnityEngine;
 
 public class bullet : MonoBehaviour
 {
-
     public float bulletspeed;
 
     Rigidbody2D rb2d;
@@ -19,5 +18,18 @@ public class bullet : MonoBehaviour
     void Update()
     {
         rb2d.MovePosition(transform.position + transform.up * bulletspeed * Time.deltaTime);  
+    }
+
+    void OnTriggerEnter (Collider other)
+    {
+        string tag = other.gameObject.tag;
+
+        Debug.Log("hsd");
+
+        if (tag == "Border")
+        {
+            Destroy(gameObject);
+            Debug.Log("hsduhdus");
+        }
     }
 }
